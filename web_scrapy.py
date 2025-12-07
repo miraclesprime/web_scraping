60359e33a324fdcdce0d774919095997cbe2e1f4
miraclesprime <miraclesprime@gmail.com>
2025-12-07 07:08:04 -0500

    removed the part
    Co-authored-by: Miracles Prime <miraclesprime@gmail.com>

--- web_scrapy.py
+++ web_scrapy.py
@@ -16,7 +16,7 @@ def cloudflare_bypass(url):
     options.add_argument('--start-maximized')
     options.add_argument('--disable-extensions')
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')sdfsdf
+    options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome(service=service, options=options)
 
