8e7579772462c190412b2b387ed10e779f83c132
miraclesprime <miraclesprime@gmail.com>
2025-12-07 07:03:59 -0500

    changed the file

--- web_scrapy.py
+++ web_scrapy.py
@@ -16,7 +16,7 @@ def cloudflare_bypass(url):
     options.add_argument('--start-maximized')
     options.add_argument('--disable-extensions')
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-dev-shm-usage')sdfsdf
 
     driver = webdriver.Chrome(service=service, options=options)
 
